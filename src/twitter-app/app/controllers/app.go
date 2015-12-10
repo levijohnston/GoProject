ebadbb78d8564d291006b107c9d3ccd9a43137c6
Levi Johnston <Levi@Levis-MacBook-Pro-2.local>
2015-12-09 16:32:00 -0800

    Friendship

--- src/twitter-app/app/controllers/app.go
+++ src/twitter-app/app/controllers/app.go
@@ -51,39 +51,49 @@ func (c App) getUser(username string) *models.User {
 }
 
 func (c App) Index() revel.Result {
-  if c.connected() != nil {
-    query := "SELECT p.* " +
+  user := c.connected()
+  if user != nil {
+    query := "SELECT p. * " +
     "FROM Post p, Friend f " +
-    "WHERE f.UserIdTwo = ? OR f.UserIdOne = ? " +
-    "AND p.UserId = f.UserIdOne OR p.UserId = f.UserIdTwo " +
+    "WHERE f.UserIdOne = ? AND p.UserId = f.UserIdTwo " + " AND f.AreFriends = ? " +
+    "OR f.UserIdTwo = ? AND p.UserId = f.UserIdOne " +  
+    "AND f.AreFriends = ? " +
     "ORDER BY PostId DESC"
-    results, err := c.Txn.Select(models.Post{}, query, c.connected().UserId, c.connected().UserId)
-
-
-   
-
-    
-
+    results, err := c.Txn.Select(models.Post{}, query, user.UserId, true, user.UserId, true)
     if err != nil {
         panic(err)
     }
-     query2 := "SELECT * from Friend f Where f.UserIdTwo = ? OR f.UserIdOne = ?"
 
-    results2, err := c.Txn.Select(models.Friend{}, query2, c.connected().UserId, c.connected().UserId)
-    fmt.Println("Connected user ", c.connected())
+    
+
+    query2 := "SELECT * from Friend f Where f.UserIdTwo = ? OR f.UserIdOne = ?"
+
+    results2, err := c.Txn.Select(models.Friend{}, query2, user.UserId, user.UserId)
+    fmt.Println("Connected user ", user)
     if err != nil {
         panic(err)
     }
     if results2 == nil{
       fmt.Println("No friends")
     }
-     for _, r := range results2 {
+     /*for _, r := range results2 {
       b := r.(*models.Friend)
+      if c.connected() !=  c.loadUserById(b.UserIdOne){
+        query3 := "SELECT * from Post WHERE PostId = ?"
+        results3, err := c.Txn.Select(models.Post{}, query3, b.UserIdOne)
+        return results3
+      }
+      else if c.connected() !=  c.loadUserById(b.UserIdTwo){
+        query3 := "SELECT * from Post WHERE PostId = ?"
+        results3, err := c.Txn.Select(models.Post{}, query3, b.UserIdTwo)
+        return results3
+      }
+
+
       fmt.Println("Friends 1",  c.loadUserById(b.UserIdOne))
       fmt.Println("Friends 2", c.loadUserById(b.UserIdTwo))
       fmt.Println("Are friends? ", b.AreFriends)
-
-     }
+     }*/
 
 
     var posts []*models.Post
